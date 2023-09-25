ae49061ba74cb81c0e151d6eead1eeaecce790ad
christiennekee <134541144+christiennekee@users.noreply.github.com>
2023-09-25 11:37:46 -0400

    Update itemModel.js
    removed other Schema

--- models/itemModel.js
+++ models/itemModel.js
@@ -22,16 +22,3 @@ const itemSchema = new mongoose.Schema({
   }, { timestamps: true })
 
   module.exports = mongoose.model('Item', itemSchema)
-  // const Item = mongoose.model('Item', itemSchema)
-  // module.exports = Item
-
-// const itemSchema = new mongoose.Schema({
-//   name: String,
-//   description: String,
-//   price: Number,
-//   quantity: Number,
-// });
-
-// const Item = mongoose.model('Item', itemSchema);
-
-// module.exports = Item
